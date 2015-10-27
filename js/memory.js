aa2d0c221fa3116caaad402993f985de637944d9
Linus Juhlin <dragonbaberiven@gmail.com>
2015-10-27 12:25:17 +0000

    Prepare

--- js/memory.js
+++ js/memory.js
@@ -1,124 +1,124 @@
 $(function() {
 
     // console.log("Hello world");
-    var result = prompt("Skriv in hur många par du vill ha");
-while(true){
-    if(result < 2 || result > 100)
-    result = prompt("Fel! skriv in igen");
-    else if (result % 1 !== 0)
-    result = prompt("Fel! skriv in igen");
-    else
-    break;
-}
-result = result*2;
-console.log(result);
-
-
-
+    var result = prompt("Skriv in hur många par du vill ha(2-100)");
+    while (true) {
+        if (result < 2 || result > 100)
+            result = prompt("Fel! skriv in igen");
+        else if (result % 1 !== 0)
+            result = prompt("Fel! skriv in igen");
+        else
+            break;
+    }
+    result = result * 2;
+    console.log(result);
+    
+    var player1 = prompt("Namn på player 1:");
+    var player2 = prompt("Namn på player 2:");
 
     var myArray = ["boll1", "boll1",
-"boll2", "boll2",
-"boll3", "boll3",
-"boll4", "boll4",
-"boll5", "boll5",
-"boll6", "boll6",
-"boll7", "boll7",
-"boll8", "boll8",
-"boll9", "boll9",
-"boll10", "boll10",
-"boll11", "boll11",
-"boll12", "boll12",
-"boll13", "boll13",
-"boll14", "boll14",
-"boll15", "boll15",
-"boll16", "boll16",
-"boll17", "boll17",
-"boll18", "boll18",
-"boll19", "boll19",
-"boll20", "boll20",
-"boll21", "boll21",
-"boll22", "boll22",
-"boll23", "boll23",
-"boll24", "boll24",
-"boll25", "boll25",
-"boll26", "boll26",
-"boll27", "boll27",
-"boll28", "boll28",
-"boll29", "boll29",
-"boll30", "boll30",
-"boll31", "boll31",
-"boll32", "boll32",
-"boll33", "boll33",
-"boll34", "boll34",
-"boll35", "boll35",
-"boll36", "boll36",
-"boll37", "boll37",
-"boll38", "boll38",
-"boll39", "boll39",
-"boll40", "boll40",
-"boll41", "boll41",
-"boll42", "boll42",
-"boll43", "boll43",
-"boll44", "boll44",
-"boll45", "boll45",
-"boll46", "boll46",
-"boll47", "boll47",
-"boll48", "boll48",
-"boll49", "boll49",
-"boll50", "boll50",
-"boll51", "boll51",
-"boll52", "boll52",
-"boll53", "boll53",
-"boll54", "boll54",
-"boll55", "boll55",
-"boll56", "boll56",
-"boll57", "boll57",
-"boll58", "boll58",
-"boll59", "boll59",
-"boll60", "boll60",
-"boll61", "boll61",
-"boll62", "boll62",
-"boll63", "boll63",
-"boll64", "boll64",
-"boll65", "boll65",
-"boll66", "boll66",
-"boll67", "boll67",
-"boll68", "boll68",
-"boll69", "boll69",
-"boll70", "boll70",
-"boll71", "boll71",
-"boll72", "boll72",
-"boll73", "boll73",
-"boll74", "boll74",
-"boll75", "boll75",
-"boll76", "boll76",
-"boll77", "boll77",
-"boll78", "boll78",
-"boll79", "boll79",
-"boll80", "boll80",
-"boll81", "boll81",
-"boll82", "boll82",
-"boll83", "boll83",
-"boll84", "boll84",
-"boll85", "boll85",
-"boll86", "boll86",
-"boll87", "boll87",
-"boll88", "boll88",
-"boll89", "boll89",
-"boll90", "boll90",
-"boll91", "boll91",
-"boll92", "boll92",
-"boll93", "boll93",
-"boll94", "boll94",
-"boll95", "boll95",
-"boll96", "boll96",
-"boll97", "boll97",
-"boll98", "boll98",
-"boll99", "boll99",
-"boll100","boll100",
+        "boll2", "boll2",
+        "boll3", "boll3",
+        "boll4", "boll4",
+        "boll5", "boll5",
+        "boll6", "boll6",
+        "boll7", "boll7",
+        "boll8", "boll8",
+        "boll9", "boll9",
+        "boll10", "boll10",
+        "boll11", "boll11",
+        "boll12", "boll12",
+        "boll13", "boll13",
+        "boll14", "boll14",
+        "boll15", "boll15",
+        "boll16", "boll16",
+        "boll17", "boll17",
+        "boll18", "boll18",
+        "boll19", "boll19",
+        "boll20", "boll20",
+        "boll21", "boll21",
+        "boll22", "boll22",
+        "boll23", "boll23",
+        "boll24", "boll24",
+        "boll25", "boll25",
+        "boll26", "boll26",
+        "boll27", "boll27",
+        "boll28", "boll28",
+        "boll29", "boll29",
+        "boll30", "boll30",
+        "boll31", "boll31",
+        "boll32", "boll32",
+        "boll33", "boll33",
+        "boll34", "boll34",
+        "boll35", "boll35",
+        "boll36", "boll36",
+        "boll37", "boll37",
+        "boll38", "boll38",
+        "boll39", "boll39",
+        "boll40", "boll40",
+        "boll41", "boll41",
+        "boll42", "boll42",
+        "boll43", "boll43",
+        "boll44", "boll44",
+        "boll45", "boll45",
+        "boll46", "boll46",
+        "boll47", "boll47",
+        "boll48", "boll48",
+        "boll49", "boll49",
+        "boll50", "boll50",
+        "boll51", "boll51",
+        "boll52", "boll52",
+        "boll53", "boll53",
+        "boll54", "boll54",
+        "boll55", "boll55",
+        "boll56", "boll56",
+        "boll57", "boll57",
+        "boll58", "boll58",
+        "boll59", "boll59",
+        "boll60", "boll60",
+        "boll61", "boll61",
+        "boll62", "boll62",
+        "boll63", "boll63",
+        "boll64", "boll64",
+        "boll65", "boll65",
+        "boll66", "boll66",
+        "boll67", "boll67",
+        "boll68", "boll68",
+        "boll69", "boll69",
+        "boll70", "boll70",
+        "boll71", "boll71",
+        "boll72", "boll72",
+        "boll73", "boll73",
+        "boll74", "boll74",
+        "boll75", "boll75",
+        "boll76", "boll76",
+        "boll77", "boll77",
+        "boll78", "boll78",
+        "boll79", "boll79",
+        "boll80", "boll80",
+        "boll81", "boll81",
+        "boll82", "boll82",
+        "boll83", "boll83",
+        "boll84", "boll84",
+        "boll85", "boll85",
+        "boll86", "boll86",
+        "boll87", "boll87",
+        "boll88", "boll88",
+        "boll89", "boll89",
+        "boll90", "boll90",
+        "boll91", "boll91",
+        "boll92", "boll92",
+        "boll93", "boll93",
+        "boll94", "boll94",
+        "boll95", "boll95",
+        "boll96", "boll96",
+        "boll97", "boll97",
+        "boll98", "boll98",
+        "boll99", "boll99",
+        "boll100", "boll100",
     ];
-    
-myArray = myArray.slice(0, result);
+
+    myArray = myArray.slice(0, result);
 
     // myArray = shuffle(myArray);
 
@@ -155,7 +155,7 @@ myArray = myArray.slice(0, result);
 
                 $("tbody").append("</td>");
                 if ((j + 1) % 8 === 0)
-                $("tbody").append("<tr></tr>");
+                    $("tbody").append("<tr></tr>");
 
                 //temp = j;
                 temp++;
@@ -209,7 +209,7 @@ myArray = myArray.slice(0, result);
                     event.preventDefault();
                     return;
                 }
-                
+
                 $(this).addClass("selected");
 
                 $(this).removeClass("bakgrund");
@@ -227,9 +227,9 @@ myArray = myArray.slice(0, result);
 
                     // If it does not contain class
                     if (!childElement.hasClass("bakgrund")) {
-                        
+
                         if (childElement.attr("value") === undefined) {
-                            
+
                             if (childElement.attr("class") === parentElement.attr("class")) {
                                 wins += 1;
 
@@ -261,10 +261,10 @@ myArray = myArray.slice(0, result);
 
                             }
                             else {
-                                
+
                                 childElement.removeClass("selected");
                                 parentElement.removeClass("selected");
-                                
+
                                 childElement.removeClass("valid");
                                 parentElement.removeClass("valid");
 
